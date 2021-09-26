0fd9868e29460e836ea710851dab3b7fbe5b098d
Le Wang-Riches <le.wang-riches@cloud-iq.com>
2021-09-26 14:49:16 +0100

    task0: updated css for hand and rules modal

--- comps/rules.tsx
+++ comps/rules.tsx
@@ -11,11 +11,20 @@ const RulesModal = ({handleClose, show}:ShowRules): ReactElement => {
     const showHideClassName = show ? 'showRules' : 'hideRules';
     return (
         <div className={styles[showHideClassName]}>
-            <section className={styles.modalMain}>
-                <Image src='/icon-close.svg' alt='close' onClick={handleClose} width='35' height='35'/>
-                <p>RULES</p>
-                <Image className={styles.rulesPicture} src='/image-rules.svg' alt='rules' width='375' height='450'/>
-            </section>
+            <div className={styles.modalMain}>
+                <div className={styles.titleContainer}>
+                    <p className={styles.titleText}>RULES</p>
+                    {/* 
+                        Please note below I had to add class to image to style their height and width
+                        I do not understand why the css is ignoring the width and height in Next/Image. 
+                        It is infact ignoring it completely and adding min/max height/width which
+                        is causing the entire style to go strange. I had to add alot of classes and !important
+                        override this feature.
+                    */}
+                    <Image src='/icon-close.svg' alt='close' onClick={handleClose} layout="fixed" width={35} height={35}/>
+                </div>
+                <Image className={styles.rulesPicture} src='/image-rules.svg' alt='rules' layout="fixed" width={325} height={350}/>
+            </div>
         </div>
     );
 }
